f5b3be6512b06329129c6b44c93342553ca747b5
Lucas Guzman <lucas_guzman@hotmail.es>
2024-02-13 10:41:02 +0100

    first commit

---
+++ mywc.c
@@ -0,0 +1,17 @@
+//P1-SSOO-23/24
+
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	/*If less than two arguments (argv[0] -> program, argv[1] -> file to process) print an error y return -1*/
+	if(argc < 2)
+	{
+		printf("Too few arguments\n");
+		return -1;
+	}
+
+	return 0;
+}
+
