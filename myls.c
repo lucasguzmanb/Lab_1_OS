674bdc7bdc3538899ad2f7da31d085afa3d61e3d
Olaya Martinez <olayamf03@gmail.com>
2024-03-04 17:31:58 +0100

    olayas part

--- myls.c
+++ myls.c
@@ -7,10 +7,36 @@
 #include <string.h>
 
 
-int main(int argc, char *argv[])
-{
-	
+int main(int argc, char *argv[]){
+    struct dirent *entry;        // to read each of the entries of the directory
+    char *dirName; // define the directory name
 
-	return 0;
+    if (argc == 1) {
+       dirName = argv[1];  //usage 1, directory given as an argument
+    }else{
+        dirName = getcwd(NULL, 0); //usage 2, open current directory
+        if (dirName == NULL){
+            return -1;
+        }
+    }
+    int dirSize = 0;
+    DIR *dir = opendir(dirName); // try to open the directory
+    if (dir == NULL){ // if there is an error opening the directory or it doesnt exist, print an error an return -1
+        printf("Error opening the directory\n");
+        return -1;
+    }else{
+        while (((entry = readdir(dir)) != NULL)&& (dirSize != 4097)) { // path size can not be greater tha 4096, 4097 counting the null
+            printf("%s\n", entry->d_name);
+            dirSize += dirSize;
+        }
+        closedir(dir);
+        printf("the current directory is %s", dirName);
+        if (chdir("..") == 0) {
+            printf("the parent directory is %s", dirName);
+        }else{
+            printf("Error getting parent directory");
+        }
+        return 0;
+    }
 }
-
+    
